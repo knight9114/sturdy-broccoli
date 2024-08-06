5a404256734919c72645d4920b2604b2be223deb
Ian Dahlstrom <ian.dahlstrom@gmail.com>
2024-08-06 06:44:21 -0400

    wip: Setting up basic CLI stuff

--- src/bin/nlp.rs
+++ src/bin/nlp.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Command;
+
+fn main() -> anyhow::Result<()> {
+    let cmd = build_cli();
+    let _matches = cmd.get_matches();
+    Ok(())
+}
+
+fn build_cli() -> clap::Command {
+    let cmd = Command::new("nlp")
+        .bin_name("nlp")
+        .subcommand_required(true);
+    let cmd = build_gpt(cmd);
+    let cmd = build_llama(cmd);
+    cmd
+}
+
+fn build_gpt(cmd: Command) -> clap::Command {
+    let gpt = Command::new("gpt");
+    cmd.subcommand(gpt)
+}
+
+fn build_llama(cmd: clap::Command) -> clap::Command {
+    let llama = Command::new("llama");
+    cmd.subcommand(llama)
 }
